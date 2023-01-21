cf255964f0a0544fb87a2c4f8dfa68c772d20e67
JulioOLV <julioccolv@gmail.com>
2023-01-20 22:52:11 -0300

    feat: adiciona value object de raças

---
+++ src/@core/domain/value-objects/race.ts
@@ -0,0 +1,13 @@
+import { EnumRaces } from "../enums/enum-races";
+import { Bonus } from "./bonus";
+import { Height } from "./height";
+import { Names } from "./names";
+
+export interface Race {
+  name: EnumRaces,
+  bonus: Bonus[],
+  maxAge: number,
+  height: Height,
+  displacement: number,
+  names: Names[],
+}
